de13e8c40de24cd1c3f990143dd83223443921ff
CPark <companymoikka@gmail.com>
2022-02-13 18:03:15 +0200

    init project

---
+++ crwn-clothing/src/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="test">HomePage</div>
+    </div>
+  );
+};
+
+export default HomePage;
