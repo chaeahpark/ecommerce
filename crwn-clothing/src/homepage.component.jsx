f9d797f92bd6fe6e07daf23fcc9d2dfc0de79dd1
CPark <companymoikka@gmail.com>
2022-02-13 20:14:48 +0200

    created project with create react app and added files for homepage.

--- crwn-clothing/src/homepage.component.jsx
+++ crwn-clothing/src/homepage.component.jsx
@@ -3,8 +3,15 @@ import "./homepage.styles.scss";
 
 const HomePage = () => {
   return (
-    <div className="container">
-      <div className="test">HomePage</div>
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
