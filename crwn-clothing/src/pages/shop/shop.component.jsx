d0c74d478adb204abbb51eb4eb6342317c8c6cae
CPark <companymoikka@gmail.com>
2022-02-15 20:51:19 +0200

    updating shop page and added collection preview and collection items

---
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -0,0 +1,31 @@
+import { getByTitle } from "@testing-library/react";
+import React from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import SHOP_DATA from "./shop.data";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map((collection) => (
+          <CollectionPreview
+            key={collection.id}
+            title={collection.title}
+            items={collection.items}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
